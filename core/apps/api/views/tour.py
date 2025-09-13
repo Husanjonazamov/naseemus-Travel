ed05cb4bf571ceec92699a0eba72e5c67ae43097
Husanjonazamov <azamovhusanboy08@gmail.com>
2025-09-13 17:31:17 +0500

    New g4f version: 0.6.2.9 (current: 0.3.9.7) | pip install -U g4f

--- core/apps/api/views/tour.py
+++ core/apps/api/views/tour.py
@@ -6,6 +6,10 @@ from rest_framework.viewsets import ModelViewSet
 from core.apps.api.models import TourModel
 from core.apps.api.serializers.tour import CreateTourSerializer, ListTourSerializer, RetrieveTourSerializer
 
+from django.db.models import Q
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 @extend_schema(tags=["tour"])
 class TourView(BaseViewSetMixin, ModelViewSet):
@@ -22,3 +26,18 @@ class TourView(BaseViewSetMixin, ModelViewSet):
         "retrieve": RetrieveTourSerializer,
         "create": CreateTourSerializer,
     }
+
+
+    @action(detail=False, methods=["get"], url_path="search")
+    def search(self, request, *args, **kwargs):
+        search_value = request.query_params.get("search")
+        qs = self.queryset
+
+        if search_value:
+            if search_value.isdigit():  # agar son bo‘lsa -> price bo‘yicha
+                qs = qs.filter(price=search_value)
+            else:  # faqat title bo‘yicha qidiradi
+                qs = qs.filter(title__icontains=search_value)
+
+        serializer = ListTourSerializer(qs, many=True)
+        return Response(serializer.data)
